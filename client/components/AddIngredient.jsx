db166a9348f420e8fd10cd9ba4b9ae4c6e284196
Valtteri Korhonen <ab3979@student.jamk.fi>
2022-12-15 15:23:53 +0200

    refactored class component to functional component

--- client/components/AddIngredient.jsx
+++ client/components/AddIngredient.jsx
@@ -1,29 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class AddIngredient extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      amount: "",
-      unit: "",
-    };
-  }
-
-  render() {
-    return (
-      <div className={"addIngredient " + this.props.visibility}>
-        <input id="name" type="text" placeholder="Raaka-aine" />
-        <input id="amount" type="number" placeholder="Määrä" />
-        <select id="unit">
-          <option value="g">g</option>
-          <option value="kg">kg</option>
-          <option value="l">l</option>
-          <option value="dl">dl</option>
-          <option value="tl">tl</option>
-          <option value="kpl">kpl</option>
-        </select>
-      </div>
-    );
-  }
+export default function AddIngredient(props) {
+  return (
+    <div className={"addIngredient " + props.visibility}>
+      <input id="name" type="text" placeholder="Raaka-aine" />
+      <input id="amount" type="number" placeholder="Määrä" />
+      <select id="unit">
+        <option value="g">g</option>
+        <option value="kg">kg</option>
+        <option value="l">l</option>
+        <option value="dl">dl</option>
+        <option value="tl">tl</option>
+        <option value="kpl">kpl</option>
+      </select>
+    </div>
+  );
 }
